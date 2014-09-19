1e986fbb865c43f2aa70c339f432b6b07984d4cd
Anandavel Murugan <ananthu2050@gmail.com>
2014-09-19 18:32:09 +0530

    Updated forceNetwork.js

--- inst/htmlwidgets/forceNetwork.js
+++ inst/htmlwidgets/forceNetwork.js
@@ -121,7 +121,25 @@ HTMLWidgets.widget({
       .style("font", options.fontsize + "px serif")
       .style("opacity", 0)
       .style("pointer-events", "none");*/
-
+  var legend = svg.selectAll(".legend")
+        .data(color.domain())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+  
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+  
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+    
     function tick() {
       link
         .attr("x1", function(d) { return d.source.x; })
